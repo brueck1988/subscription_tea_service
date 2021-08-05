3bda005efb36f2ecfe4f5943dbb9f85074548dc5
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-08-04 18:24:11 -0600

    Add validations and relationships

--- app/models/customer_subscription.rb
+++ app/models/customer_subscription.rb
@@ -1,9 +1,6 @@
 class CustomerSubscription < ApplicationRecord
-  validates_presence_of :first_name
-  validates_presence_of :last_name
-  validates_presence_of :email
-  validates_presence_of :address
-  
   belongs_to :customer
   belongs_to :subscription
+  has_many :subscription_teas, through: :subscription
+  has_many :teas, through: :subscription_teas
 end
--- app/models/subscription_tea.rb
+++ app/models/subscription_tea.rb
@@ -1,4 +1,6 @@
 class SubscriptionTea < ApplicationRecord
   belongs_to :subscription
   belongs_to :tea
+  has_many :customer_subscriptions, through: :subscription
+  has_many :customers, through: :customer_subscriptions
 end
